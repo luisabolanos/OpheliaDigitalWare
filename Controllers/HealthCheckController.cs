f040b3698fd550ebe8b2c7736888faad1612f874
LuisaBolanos <luisa.bolanos@cjtytsoftware.com>
2020-12-10 19:29:08 -0500

    Initial commit

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OpheliaDigitalWare.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly ILogger<HealthCheckController> logger;
+        private IConfiguration configuration;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public OkObjectResult Get()
+        {
+            this.logger.LogInformation("[GET] init get");
+
+            String version =  this.configuration.GetValue<String>("Version");
+
+            return Ok(new { version = version });
+        }   
+    }
+}
